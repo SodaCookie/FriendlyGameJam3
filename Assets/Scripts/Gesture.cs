89cae2b9b5650af67217f2373d40237fab72d8aa
gjh33 <manhappylife@yahoo.ca>
2018-03-02 21:18:50 -0500

    First commit with joycon shit

---
+++ Assets/Scripts/Gesture.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gesture")]
+public class Gesture : ScriptableObject {
+    public string gestureID;
+    public string dataLoadPath;
+
+    [HideInInspector]
+    public List<DataPoint> data;
+
+    // Loads data from path relative to StreamingAssetsPath
+    public void LoadData()
+    {
+        string fullPath = Path.Combine(Application.streamingAssetsPath, dataLoadPath);
+
+        if (File.Exists(fullPath))
+        {
+            string loadedJson = File.ReadAllText(fullPath);
+            DataSerializationContainer loadedData = JsonUtility.FromJson<DataSerializationContainer>(loadedJson);
+            data = new List<DataPoint>(loadedData.dataArray);
+            Debug.Log("Successfully loaded gesture " + gestureID);
+
+        }
+        else
+        {
+            Debug.LogError("Failed to load gesture data for " + gestureID);
+        }
+
+    }
+
+    // Saves the data to path relative to StreamingAssetsPath
+    public void SaveData()
+    {
+        //string fullPath = Path.Combine(Application.streamingAssetsPath, dataLoadPath);
+        string fullPath = Application.streamingAssetsPath + "/" + dataLoadPath;
+
+        string jsonData = JsonUtility.ToJson(new DataSerializationContainer(data));
+        File.WriteAllText(fullPath, jsonData);
+        Debug.Log("Successfully saved gesture " + gestureID + " to " + fullPath);
+    }
+
+    // Helper Struct
+    [System.Serializable]
+    public struct DataPoint
+    {
+        public Vector3 RAccelerometerData;
+        public Vector3 LAccelerometerData;
+        public Vector3 RGyroscopeData;
+        public Vector3 LGyroscopeData;
+        public float time;
+
+        public DataPoint(Vector3 AccelDataLeft, Vector3 AccelDataRight, Vector3 GyroDataLeft, Vector3 GyroDataRight, float t)
+        {
+            LAccelerometerData = AccelDataLeft;
+            RAccelerometerData = AccelDataRight;
+
+            LGyroscopeData = GyroDataLeft;
+            RGyroscopeData = GyroDataRight;
+
+            time = t;
+        }
+    }
+
+    [System.Serializable]
+    struct DataSerializationContainer
+    {
+        public DataPoint[] dataArray;
+
+        public DataSerializationContainer(DataPoint[] pDataArray)
+        {
+            dataArray = pDataArray;
+        }
+
+        public DataSerializationContainer(List<DataPoint> pDataList)
+        {
+            dataArray = pDataList.ToArray();
+        }
+    }
+}
---
+++ Assets/Scripts/GestureRecorder.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class GestureRecorder : MonoBehaviour {
+    public Gesture gesture;
+
+    // Grabbed automatically from Joycon Manager
+    private Joycon leftJoycon;
+    private Joycon rightJoycon;
+
+    private float recordingStartTime = 0;
+    private bool recording = false;
+    private List<Gesture.DataPoint> recordingBuffer = new List<Gesture.DataPoint>();
+
+	// Use this for initialization
+	void Start () {
+		// Grab joycons from JoyconManager
+        foreach (Joycon jc in JoyconManager.Instance.j)
+        {
+            if (jc.isLeft) leftJoycon = jc;
+            if (!jc.isLeft) rightJoycon = jc;
+        }
+
+        if (leftJoycon == null || rightJoycon == null)
+        {
+            Debug.LogError("Unable to find left and right joycon!");
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (recording) RecordCurrentInputs();
+        handleInput();
+	}
+
+    public void BeginRecording()
+    {
+        recordingBuffer.Clear();
+        recordingStartTime = Time.time;
+        recording = true;
+    }
+
+    public void EndRecording()
+    {
+        recording = false;
+        gesture.data = recordingBuffer;
+        gesture.SaveData();
+    }
+
+    public void RecordCurrentInputs()
+    {
+        Vector3 rA = rightJoycon.GetAccel();
+        Vector3 lA = leftJoycon.GetAccel();
+        Vector3 rG = rightJoycon.GetGyro();
+        Vector3 lG = leftJoycon.GetGyro();
+        float time = Time.time - recordingStartTime;
+
+        Gesture.DataPoint dp = new Gesture.DataPoint(lA, rA, lG, rG, time);
+
+        recordingBuffer.Add(dp);
+    }
+
+    private void handleInput()
+    {
+        // If both buttons are pressed
+        if (rightJoycon.GetButton(Joycon.Button.SR) && leftJoycon.GetButton(Joycon.Button.SL))
+        {
+            // Begin recording
+            if (!recording)
+            {
+                BeginRecording();
+                Debug.Log("Start Recording");
+            }
+        }
+        // If the buttons are let go
+        else
+        {
+            // Stop recording
+            if (recording)
+            {
+                EndRecording();
+                Debug.Log("End Recording");
+            }
+        }
+    }
+}
