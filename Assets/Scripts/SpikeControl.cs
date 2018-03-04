d9f6260ba90ddbde1309e1eca5a719d73d80f899
SodaCookie <xzeric@hotmail.com>
2018-03-04 17:12:10 -0500

    UPgraded visuals of tutorial levels Added spike prefab

---
+++ Assets/Scripts/SpikeControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeControl : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		PlayerControls controls = other.GetComponent<PlayerControls> ();
+		if (controls != null) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+	}
+}
